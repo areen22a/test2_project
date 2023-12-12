5fe8985960df587be0beb3c6cf49aa96b6d03134
El-Gazoly <elgazoly11@gmail.com>
2023-12-12 15:23:47 +0200

    fetch data from firebase

--- src/component/Music.js
+++ src/component/Music.js
@@ -1,7 +1,32 @@
 import React from 'react'
-
+import { getMusic } from '../api/music'
+import {
+    useQuery
+  } from '@tanstack/react-query'
 
 const Music = () => {
+    const {data, isLoading, error} = useQuery({ queryKey: ['music'], queryFn: getMusic })
+    if(isLoading) console.log("loading")
+    if(data) console.log(data)
+    if(error) console.log("error", error)
+
+    const getVideoIdFromLink = (link) => {
+        let videoId = '';
+        const url = new URL(link);
+      
+        if (url.hostname === 'www.youtube.com' || url.hostname === 'youtube.com') {
+          if (url.search) {
+            const params = new URLSearchParams(url.search);
+            videoId = params.get('v');
+          } else {
+            videoId = url.pathname.split('/').pop();
+          }
+        } else if (url.hostname === 'youtu.be') {
+          videoId = url.pathname.substr(1);
+        }
+        const embedUrl = `https://www.youtube.com/embed/${videoId}`;
+        return embedUrl;
+      };
 
     // Start Play Video
     const playVideo = (e) =>  {
@@ -28,111 +53,38 @@ const Music = () => {
             </div>
 
             <div className="row row-col-5">
+            {data && data.map((music) => (
+            <div className="col" key={music.link}>
+            <div className="card">
 
-                <div className="col">
-                    <div className="card">
-
-                        <div className="card-image">
-                        <iframe
+                <div className="card-image">
+                <iframe
                 // width="560"
                 // height="315"
-                src="https://www.youtube-nocookie.com/embed/qnqTkoPm2yU?si=eTkmmVpDveijS5vt"
+                src={getVideoIdFromLink(music.link)}
                 title="YouTube video player"
                 frameborder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                 allowfullscreen
-              ></iframe>
+                ></iframe>
 
-                                <div className="play-icon position-absolute top-50 start-50 translate-middle" onClick={(e) => playVideo(e)}> <a className=" icon-md bg-danger text-white rounded-circle" href="video-details.html"> <i class="bi bi-play-fill fs-5"> </i> </a>
-                            </div>
-                            <div className="position-absolute bottom-0 start-0 p-3 d-flex w-100">
-                                <span className="bg-dark bg-opacity-50 px-2 rounded text-white small">03:45</span>
-                                <span className="bg-dark bg-opacity-50 px-2 rounded text-white small ms-auto heart"><i className="fa-solid fa-heart" onClick={(e) => addToFavorite(e)}></i></span>
-                            </div> 
+                            <div className="play-icon position-absolute top-50 start-50 translate-middle" onClick={(e) => playVideo(e)}> <a className=" icon-md bg-danger text-white rounded-circle" href="video-details.html"> <i class="bi bi-play-fill fs-5"> </i> </a>
                         </div>
+                        <div className="position-absolute bottom-0 start-0 p-3 d-flex w-100">
+                            <span className="bg-dark bg-opacity-50 px-2 rounded text-white small">{music.Duration}</span>
+                            <span className="bg-dark bg-opacity-50 px-2 rounded text-white small ms-auto heart"><i className="fa-solid fa-heart" onClick={(e) => addToFavorite(e)}></i></span>
+                        </div> 
+                    </div>
 
-                        <div className="card-body">
-                            <h3 className="card-title fs-6">New movie trailers (2021 -2022) September</h3>
-                            <span className="small"> 325.1K views</span>
-                        </div>
+                    <div className="card-body">
+                        <h3 className="card-title fs-6">{music.title}</h3>
+                        <span className="small"> {music.views}</span>
                     </div>
                 </div>
-                <div className="col">
-                    <div className="card">
-
-                        <div className="card-image">
-
-                            <video  src="images/music/12.mp4" alt="" className="card-img-top" />
-                                <div className="play-icon position-absolute top-50 start-50 translate-middle" onClick={(e) => playVideo(e)}><i className="fa-solid fa-play"></i> </div>
-                            <div className="position-absolute bottom-0 start-0 p-3 d-flex w-100">
-                                <span className="bg-dark bg-opacity-50 px-2 rounded text-white small">03:45</span>
-                                <span className="bg-dark bg-opacity-50 px-2 rounded text-white small ms-auto heart"><i className="fa-solid fa-heart" onClick={(e) => addToFavorite(e)}></i></span>
-                            </div> 
-                        </div>
-
-                        <div className="card-body">
-                            <h3 className="card-title fs-6">New movie trailers (2021 -2022) September</h3>
-                            <span className="small"> 325.1K views</span>
-                        </div>
-                    </div>
                 </div>
-                <div className="col">
-                    <div className="card">
-
-                        <div className="card-image">
-
-                            <video  src="images/music/11.mp4" alt="" className="card-img-top" />
-                                <div className="play-icon position-absolute top-50 start-50 translate-middle" onClick={(e) => playVideo(e)}><i className="fa-solid fa-play"></i> </div>
-                            <div className="position-absolute bottom-0 start-0 p-3 d-flex w-100">
-                                <span className="bg-dark bg-opacity-50 px-2 rounded text-white small">03:45</span>
-                                <span className="bg-dark bg-opacity-50 px-2 rounded text-white small ms-auto heart"><i className="fa-solid fa-heart" onClick={(e) => addToFavorite(e)}></i></span>
-                            </div> 
-                        </div>
-
-                        <div className="card-body">
-                            <h3 className="card-title fs-6">New movie trailers (2021 -2022) September</h3>
-                            <span className="small"> 325.1K views</span>
-                        </div>
-                    </div>
-                </div>
-                <div className="col">
-                    <div className="card">
-
-                        <div className="card-image">
-
-                            <video  src="images/music/12.mp4" alt="" className="card-img-top" />
-                                <div className="play-icon position-absolute top-50 start-50 translate-middle" onClick={(e) => playVideo(e)}><i className="fa-solid fa-play"></i> </div>
-                            <div className="position-absolute bottom-0 start-0 p-3 d-flex w-100">
-                                <span className="bg-dark bg-opacity-50 px-2 rounded text-white small">03:45</span>
-                                <span className="bg-dark bg-opacity-50 px-2 rounded text-white small ms-auto heart"><i className="fa-solid fa-heart" onClick={(e) => addToFavorite(e)}></i></span>
-                            </div> 
-                        </div>
-
-                        <div className="card-body">
-                            <h3 className="card-title fs-6">New movie trailers (2021 -2022) September</h3>
-                            <span className="small"> 325.1K views</span>
-                        </div>
-                    </div>
-                </div>
-                <div className="col">
-                    <div className="card">
-
-                        <div className="card-image">
-
-                            <video  src="images/music/11.mp4" alt="" className="card-img-top" />
-                                <div className="play-icon position-absolute top-50 start-50 translate-middle" onClick={(e) => playVideo(e)}><i className="fa-solid fa-play"></i> </div>
-                            <div className="position-absolute bottom-0 start-0 p-3 d-flex w-100">
-                                <span className="bg-dark bg-opacity-50 px-2 rounded text-white small">03:45</span>
-                                <span className="bg-dark bg-opacity-50 px-2 rounded text-white small ms-auto heart"><i className="fa-solid fa-heart" onClick={(e) => addToFavorite(e)}></i></span>
-                            </div> 
-                        </div>
-
-                        <div className="card-body">
-                            <h3 className="card-title fs-6">New movie trailers (2021 -2022) September</h3>
-                            <span className="small"> 325.1K views</span>
-                        </div>
-                    </div>
-                </div>
+                ))}
+              
+            
             </div>
 
         </div>
